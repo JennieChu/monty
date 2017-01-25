efee2e312813caef800dd74ed95104ad25f26be7
Jennie Chu <jenniechu92@gmail.com>
2017-01-25 23:19:15 +0000

    added wrong bytecode implementation

--- get_opcode_func.c
+++ get_opcode_func.c
@@ -18,11 +18,10 @@ void (*get_opcode_func(char *s))(stack_t **stack, unsigned int ln)
 		{"swap", stk_swap},
 		{"pint", stk_pint},
 		{"nop", stk_nop},
-		{NULL, NULL}
 	};
 
 	i = 0;
-	while (i < 8)
+	while (i < 7)
 	{
 		if (strcmp(s, (stk[i]).opcode) == 0)
 		{
